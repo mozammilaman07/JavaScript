9b15ffb31f36fe07c4f0c618f13467a028681c2b
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-11-01 22:27:57 +0530

    guess movie name

---
+++ script.js
@@ -0,0 +1,8 @@
+let favMovie = "Tiger";
+let guess = prompt("Enter fav movie name");
+// let quit = "quit";
+// while((guess != favMovie)&&(guess != quit)){
+//     console.log("Try again");
+//     guess = prompt("Enter fav movie name");
+// }
+// console.log("Great");
